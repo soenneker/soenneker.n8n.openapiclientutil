e81ac78a24f10478dca63a9bb37b39e9eb495044
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:24 -0500

    xml commenting

--- src/Soenneker.N8n.OpenApiClientUtil/Abstract/IN8nOpenApiClientUtil.cs
+++ src/Soenneker.N8n.OpenApiClientUtil/Abstract/IN8nOpenApiClientUtil.cs
@@ -10,5 +10,10 @@ namespace Soenneker.N8n.OpenApiClientUtil.Abstract;
 /// </summary>
 public interface IN8nOpenApiClientUtil: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<N8nOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
